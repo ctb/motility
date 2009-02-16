4108d44ca2320b579e2fd34b9a9d874777fff4b0
t <t@d855f457-450e-0410-a4f6-b2c5563b6eec>
2009-02-16 16:49:59 +0000

    whoops, fixed to use eq 4, Stormo, 2000.

--- examples/bg_nt.py
+++ examples/bg_nt.py
@@ -1,6 +1,6 @@
 """
-A simple example of one way to correct for background nt frequencies in
-PWM calculations.
+A simple example of a way to correct for background nt frequencies in
+PWM calculations by biasing against more common sites.
 """
 
 import motility
@@ -12,51 +12,62 @@ class BackgroundAdjustedPWM(motility.PWM):
 
 default_frequencies = dict(A=.25, C=.25, G=.25, T=.25)
 def make_bg_pwm(sites, frequencies=default_frequencies):
+    """
+    Build a PWM according to Stormo, 2000, eqn 4; modify f_{b,i} with
+    pseudocounts by adding +1.
+    """
     # validate 'frequencies' dict
     if len(frequencies.keys()) != 4 or sum(frequencies.values()) != 1.0 \
        or not ('A' in frequencies and 'G' in frequencies and \
                'C' in frequencies and 'T' in frequencies):
         raise Exception("passed in frequencies dict must have values for A,C,G,T only, and the values must sum to 1")
-    
-    # x = base count, y = max count
-    cell_fn = lambda x, y, z: math.log((float(x) + 1) / (float(y) + 1), 2)
-    matrix = motility.make_matrix(sites, cell_fn, None, normalize=True)
 
-    # now, adjust matrix for observed frequencies of nts in genome:
-    adjusted = []
-    for (a, c, g, t) in matrix:
-        a *= 0.25 / float(frequencies['A'])
-        c *= 0.25 / float(frequencies['C'])
-        g *= 0.25 / float(frequencies['G'])
-        t *= 0.25 / float(frequencies['T'])
+    a_freq = float(frequencies['A'])
+    c_freq = float(frequencies['C'])
+    g_freq = float(frequencies['G'])
+    t_freq = float(frequencies['T'])
 
-        adjusted.append((a, c, g, t))
+    ###
 
-    return BackgroundAdjustedPWM(adjusted)
+    base_counts = motility.build.count_bases_by_position(sites)
+
+    matrix = []
+    for d in base_counts:
+        column = [d[ch] for ch in 'ACGT']
+        a, c, g, t = column
+        n_sites = sum(column)
+
+        a = math.log(float(a + 1) / float(n_sites + 1) / a_freq, 2)
+        c = math.log(float(c + 1) / float(n_sites + 1) / c_freq, 2)
+        g = math.log(float(g + 1) / float(n_sites + 1) / g_freq, 2)
+        t = math.log(float(t + 1) / float(n_sites + 1) / t_freq, 2)
+
+        column = [ a, c, g, t ]
+        
+        # normalize
+        norm_factor = min(column)
+        column = [ x - norm_factor for x in column ]
+        matrix.append(column)
+
+    return BackgroundAdjustedPWM(matrix)
 
 ###
 
 sites = [ 'AGATAA',
-          'TGATAA',
           'AGATAG' ]
 
-pwm = motility.make_pwm(sites)
+# if AT is more common than GC, the PWM should score a GC-rich site
+# higher because it is less likely to occurr at random.
 
-##
-
-bg_pwm = make_bg_pwm(sites)
-
-# equal, if no frequency dict given.
-assert pwm.calc_score(sites[0]) == bg_pwm.calc_score(sites[0])
-
-##
-
+pwm = make_bg_pwm(sites)
 bg_pwm = make_bg_pwm(sites, dict(A=.35, C=.15, G=.15, T=.35))
 
-# should score AT-rich sites lower, and GC-rich sites higher
-site = 'AAAAAA'
-assert bg_pwm.calc_score(site) < pwm.calc_score(site)
+site = 'AGATAA'
+site2 = 'AGATAG'
 
-site = 'GGGGGG'
-assert bg_pwm.calc_score(site) > pwm.calc_score(site)
+# the scores under the AT==GC model should be equivalent
+assert pwm.calc_score(site) == pwm.calc_score(site2)
 
+# the score for 'site' (higher AT) should be less than the score for
+# 'site2' (higher GC).
+assert bg_pwm.calc_score(site) < bg_pwm.calc_score(site2)
